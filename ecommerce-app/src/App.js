b0c75d08678880df38a90b12eed0c5be5d1d73bb
colloso999 <tripperskripper@gmail.com>
2024-01-20 13:42:39 +0300

    Refactor routing in App component

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -1,20 +1,29 @@
+// Importing necessary modules from React and react-router-dom
 import React from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+
+// Importing custom components
 import Layout from './components/Layout';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import About from './Pages/About';
 
+// Defining the main App component
 function App() {
 return (
   <>
+  {/* Wrapping the entire application in BrowserRouter */}
   <BrowserRouter>
   <Routes>
+    {/* Defining the main layout */}
     <Route path='/' element={<Layout />}>
+      {/* Route for Home page */}
       <Route index element={<Home />}/>
+      {/* Route for about page */}
       <Route path='about' element={<About />}/>
+      {/* Route for contact page */}
       <Route path='contact' element={<Contact />}/>
     </Route>
   </Routes>
@@ -23,4 +32,5 @@ return (
 );
 }
 
+// Exporting the App component as the default export
 export default App;
