3c74e2aebd9e47232dd6a904e2034a6c9c8d4519
colloso999 <tripperskripper@gmail.com>
2024-01-23 09:51:52 +0300

    Contact component

---
+++ ecommerce-app/src/Pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * Contact component.
+ * 
+ * @component
+ * @return {JSX.Element} The rendered Contact component
+ */
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
