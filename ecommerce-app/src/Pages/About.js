1788c9ef29f413fe63c2e203b7bf25497e64d53b
colloso999 <tripperskripper@gmail.com>
2024-01-23 09:49:34 +0300

    About component

---
+++ ecommerce-app/src/Pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+/**
+ * About component.
+ * 
+ * @component
+ * @return {JSX.Element} The rendered About component.
+ */
+const About = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default About
