abc0d6270f0d2beffabd403d84d497869845728e
colloso999 <tripperskripper@gmail.com>
2024-01-25 23:25:42 +0300

    OurStore component

---
+++ ecommerce-app/src/Pages/OurStore.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/**
+ * 
+ * Our Store Component.
+ * 
+ * @component
+ * @return {JSX.Element} The rendered Store component
+ */
+const OurStore = () => {
+  return (
+    <div>OurStore</div>
+  )
+}
+
+export default OurStore
